34dc4951b060d94a28ff00c267a2bdc756088023
Warryz <8317771+Warryz@users.noreply.github.com>
2018-10-04 23:57:44 +0200

    Added control flow for no delays.

--- main.py
+++ main.py
@@ -7,19 +7,23 @@ def main():
     parser = enno_train_delay.extract_train_delay_information()
     table_content = parser.parse_train_delay_table(url)
 
-    for line in table_content:
-        # Contains header
-        if 'Datum' in line and 'Uhrzeit' in line and 'Meldung' in line:
-            # Nothing yet...
-            pass
+    # This should only be true if there are no delays.
+    if len(table_content) == 2:
+        print('Keine Meldungen vorhanden!')
+    else:
+        for line in table_content:
+            # Contains header
+            if 'Datum' in line and 'Uhrzeit' in line and 'Meldung' in line:
+                # Nothing yet...
+                pass
 
-        # Line contains the beautiful data we want
-        else:
-            date = parser.parse_train_delay_table_date(line)
-            time = parser.parse_train_delay_table_time(line)
-            msg = parser.parse_train_delay_table_msg(line)
+            # Line contains the beautiful data we want
+            else:
+                date = parser.parse_train_delay_table_date(line)
+                time = parser.parse_train_delay_table_time(line)
+                msg = parser.parse_train_delay_table_msg(line)
 
-            print('{} um {}: {}'.format(date, time, msg))
+                print('{} um {}: {}'.format(date, time, msg))
 
 
 if __name__ == '__main__':
